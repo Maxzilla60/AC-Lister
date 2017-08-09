10cc4f2774be3c1973d5279fef20d331ecbbd65d
Maxime Orione <maxpower98@hotmail.com>
2017-08-09 22:35:46 +0200

    Moved icon and fixed empty list in viewer

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -22,7 +22,7 @@ function viewLists() {
     }
     // In case of empty list:
     else {
-        block = "<div style=\"padding-left:15px;color:orange;\">Press the<i onclick=\"newList();\" title=\"Add list\" class=\"clickable fa fa-plus\" aria-hidden=\"true\" style=\"margin-left:3px;margin-right:3px;\"></i>to make a new list!</div>";
+        block = "<div style=\"padding-left:15px;color:orange;\">Press the<i onclick=\"closeViewer();\" title=\"Close viewer\" class=\"clickable fa fa-compress\" aria-hidden=\"true\" style=\"margin-left:3px;margin-right:3px;\"></i>and make a list!</div>";
     }
     
     // Display block
