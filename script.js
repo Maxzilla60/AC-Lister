2be9fecce5ea6baad28092d96828f846bd2e0d27
Maxime Orione <maxpower98@hotmail.com>
2017-08-09 12:03:28 +0200

    Workaround for duplicate names

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function viewResults(resultList) {
     document.getElementById("search_results").innerHTML = "";
     
     for (var v in resultList) {
+        trimmedName = trimName(resultList[v].name);
+        
         block = "<div onclick=\"loadProfile('" + resultList[v].name + "')\" class=\"result\">" +
         "<img style=\"float:left\" src=\"villager_icons/" + resultList[v].name + ".gif\">" +
-        "<div>" + resultList[v].name + "</div>" +
+        "<div>" + trimmedName + "</div>" +
         "</div><br>";
         document.getElementById("search_results").innerHTML += block;
     }
@@ -34,21 +36,22 @@ function loadProfile(name) {
     // TODO find in list
     //name = "Scoot";
     villager = getVillager(name);
+    trimmedName = trimName(name);
     var personality = villager.personality;
     var coffee = villager.coffee;
     var birthday = villager.birthday;
     
-    var icon_wiki = "<i onclick=\"window.open('http://animalcrossing.wikia.com/wiki/" + name + "','_blank')\" title=\"Open Wiki page\" class=\"clickable fa fa-wikipedia-w\" style=\"float:right;\" aria-hidden=\"true\"></i>";
+    var icon_wiki = "<i onclick=\"window.open('http://animalcrossing.wikia.com/wiki/" + trimmedName + "','_blank')\" title=\"Open Wiki page\" class=\"clickable fa fa-wikipedia-w\" style=\"float:right;\" aria-hidden=\"true\"></i>";
     var icon_name = "<i title=\"Name\" class=\"fa fa-user\" aria-hidden=\"true\"></i>";
     var icon_personality = "<i title=\"Personality\" class=\"fa fa-heart\" aria-hidden=\"true\"></i>";
     var icon_coffee = "<i title=\"Favorite coffee\" class=\"fa fa-coffee\" aria-hidden=\"true\"></i>";
     var icon_birthday = "<i title=\"Birthday\" class=\"fa fa-birthday-cake\" aria-hidden=\"true\"></i>";
-    var icon_add = "<i onclick=\"addVillager();\" title=\"Add to list\" class=\"clickable fa fa-plus\" aria-hidden=\"true\"></i>";
+    var icon_add = "<i onclick=\"addVillager(\"" + name + "\");\" title=\"Add to list\" class=\"clickable fa fa-plus\" aria-hidden=\"true\"></i>";
     var options = "<option>Favorites</option><option>Hyrule</option>"; // TODO
     var br = "<br>";
     
     block = icon_wiki + "<img src=\"villager_heads/wip.jpg\" class=\"profile-image\">" + "<div class=\"profile\">" +
-        icon_name + name + br +
+        icon_name + trimmedName + br +
         icon_personality + personality + br +
         icon_coffee + coffee + br +
         icon_birthday + birthday + br + br +
@@ -96,6 +99,13 @@ function search(query) {
     }
     
     viewResults(results);
+}
+
+function trimName(name) {
+    if (name.includes("(2)")) {
+        return name.replace(" (2)", "");
+    }
+    return name;
 }
 
 function searchbarLoading() {
