e8133574195b6705543c1464c8769e4ec4248813
Maxime Orione <maxpower98@hotmail.com>
2017-08-09 13:48:03 +0200

    Implemented dynamic loading of lists

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+var lists = [
+    {
+        title: "Favorites",
+        id: 0,
+        members: [
+            "Scoot",
+            "Simon",
+            "Petunia (2)"
+        ]
+    },
+    {
+        title: "Hyrule",
+        id: 1,
+        members: [
+            "Peanut",
+            "Goose"
+        ]
+    }
+];
+
+function viewLists() {
+    document.getElementById("lists").innerHTML = "";
+    
+    var block = "<div class=\"lists\">";
+    for (var list in lists) {
+        block += "<div style=\"display:inline-block;\">" + lists[list].title + "</div>" +
+            "<i onclick=\"deleteList(" + lists[list].id + ")\"  title=\"Delete list\" class=\"clickable fa fa-minus\" aria-hidden=\"true\"></i>" +
+            "<i onclick=\"renameList(" + lists[list].id + ")\"  title=\"Edit name\" class=\"clickable fa fa-pencil\" aria-hidden=\"true\"></i><div style=\"padding-bottom:0;padding-top:0;\">";
+        for (var member in lists[list].members) {
+            for (var i = 0 ; i < 30 ; i++) {
+            trimmedName = trimName(lists[list].members[member])
+            block += "<img onclick=\"loadProfile('" + lists[list].members[member] + "')\" title=\"" + trimmedName + "\" src=\"villager_icons/" + lists[list].members[member] + ".gif\">";
+            }
+        }
+        block += "</div>";
+    }
+    block += "</div>";
+    
+    document.getElementById("lists").innerHTML = block;
+}
+
+/*
+<div class="list">
+    <div style="display:inline-block;">Favorites</div>
+    <i onclick="deleteList(0)" title="Delete list" class="clickable fa fa-minus" aria-hidden="true"></i><i title="Edit name" class="clickable fa fa-pencil" aria-hidden="true"></i>
+    <div>
+        <img onclick="removeVillager(0,'Scoot')" title="Scoot" src="villager_icons/Scoot.gif">
+        <img onclick="removeVillager(0,'Scoot')" title="Scoot" src="villager_icons/Scoot.gif">
+        <img onclick="removeVillager(0,'Scoot')" title="Scoot" src="villager_icons/Scoot.gif">
+    </div>
+</div>
+*/
+
 // Display search results in results bar
 function viewResults(resultList) {
     document.getElementById("search_results").innerHTML = ""; // Clear results bar
@@ -6,7 +59,7 @@ function viewResults(resultList) {
         trimmedName = trimName(resultList[v].name); // Trim name for duplicate names
         
         // Create html block:
-        block = "<div onclick=\"loadProfile('" + resultList[v].name + "')\" class=\"result\">" +
+        var block = "<div onclick=\"loadProfile('" + resultList[v].name + "')\" class=\"result\">" +
         "<img style=\"float:left\" src=\"villager_icons/" + resultList[v].name + ".gif\">" +
         "<div>" + trimmedName + "</div>" +
         "</div><br>";
@@ -15,6 +68,7 @@ function viewResults(resultList) {
     }
 }
 
+// Display villager profile
 function loadProfile(name) {
     villager = getVillager(name); // Get villager json
     trimmedName = trimName(name); // Trim name for duplicate names
@@ -29,7 +83,7 @@ function loadProfile(name) {
     var icon_personality = "<i title=\"Personality\" class=\"fa fa-heart\" aria-hidden=\"true\"></i>";
     var icon_coffee = "<i title=\"Favorite coffee\" class=\"fa fa-coffee\" aria-hidden=\"true\"></i>";
     var icon_birthday = "<i title=\"Birthday\" class=\"fa fa-birthday-cake\" aria-hidden=\"true\"></i>";
-    var icon_add = "<i onclick=\"addVillager(\"" + name + "\");\" title=\"Add to list\" class=\"clickable fa fa-plus\" aria-hidden=\"true\"></i>";
+    var icon_add = "<i onclick=\"addVillager('" + name + "');\" title=\"Add to list\" class=\"clickable fa fa-plus\" aria-hidden=\"true\"></i>";
     // Other blocks:
     var options = "<option>Favorites</option><option>Hyrule</option>"; // TODO
     var br = "<br>";
@@ -104,3 +158,4 @@ function searchbarLoading() {
 
 // on page load:
 viewResults(villagers);
+viewLists();
