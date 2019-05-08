d83c951e96b1e8380a95b7f9594ef3a1dda41207
Maxime Orione <maxpower98@hotmail.com>
2019-05-08 08:55:15 +0200

    ⚗ Playing around with importing JSON

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const villagersJson = require('./villagers.json');
+
+let villagers = villagersJson;
+
+// villagers = villagers.map(v => v.birthday);
+// villagers = villagers.filter(v => v.birthday === '');
+// villagers = villagers.map(v => v.split(' ')[0]);
+villagers = villagers.map(v => v.personality);
+villagers = [...new Set(villagers)];
+
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = "Hello";
+    element.innerHTML = villagers.join('<br/>');
 
     return element;
 }
 
 document.body.appendChild(component());
+console.log(villagers);
