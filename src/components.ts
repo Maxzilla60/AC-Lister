a4bd71b4e32650bbc9e13fd4753e4b6fe26d47a0
Maxime Orione <maxpower98@hotmail.com>
2019-05-10 09:49:07 +0200

    🚚 Code splitting

--- src/components.ts
+++ src/components.ts
@@ -1,4 +1,4 @@
-import { addVillager, applyTitle, currentProfile, deleteList, getListSelectValue, listsAreEmpty, loadProfile, removeVillager, renameList, updateListSelect } from './main';
+import { addVillager, applyTitle, birthdayIsToday, currentProfile, deleteList, getListSelectValue, listsAreEmpty, loadProfile, removeVillager, renameList, updateListSelect } from './main';
 import { Villager } from './models/villager.model';
 import { VillagerList } from './models/villagerlist.model';
 
@@ -154,4 +154,111 @@ export function aVillagersSearchResultImage(villager: Villager): HTMLImageElemen
     villagersSearchResultImage.style.cssFloat = 'left';
     villagersSearchResultImage.src = `./villager_icons/${villager.id}.gif`;
     return villagersSearchResultImage;
+}
+
+export function anAddOrRemoveElement(): HTMLElement {
+    let addOrRemoveElement: HTMLElement = document.createElement('div');
+    addOrRemoveElement.style.padding = '0';
+    addOrRemoveElement.style.display = 'inline-block';
+    return addOrRemoveElement;
+}
+
+export function aBreakElement(): HTMLElement {
+    return document.createElement('br');
+}
+
+export function aTextNode(text: string): Text | HTMLSpanElement {
+    return text === '' ? anNASpanElement() : document.createTextNode(text);
+}
+
+export function anNASpanElement(): HTMLSpanElement {
+    let naElement: HTMLSpanElement = document.createElement('span');
+    naElement.className = 'na';
+    naElement.innerHTML = 'N/A';
+    return naElement;
+}
+
+export function aBirthdayTextNode(birthday: Date): Text | HTMLSpanElement {
+    if (birthdayIsToday(birthday.toString())) {
+        let birthdaySpan: HTMLSpanElement = document.createElement('span');
+        birthdaySpan.innerHTML = birthday.toString();
+        birthdaySpan.className = 'birthday';
+        return birthdaySpan;
+    } else {
+        return aTextNode(birthday.toString());
+    }
+}
+
+export function aBirthdayButton(villagerName: string): HTMLButtonElement {
+    let birthdayEasterEggButton: HTMLButtonElement = document.createElement('button');
+    birthdayEasterEggButton.title = `Happy birthday to ${villagerName}!`;
+    birthdayEasterEggButton.onclick = () => {
+        new Audio('./happybirthday.mp3').play();
+    };
+    birthdayEasterEggButton.style.color = 'hotpink';
+    birthdayEasterEggButton.className = 'clickable fa fa-birthday-cake';
+    birthdayEasterEggButton.setAttribute('aria-hidden', 'true');
+    return birthdayEasterEggButton;
+}
+
+export function aWikiIconButton(wikiLink: string): HTMLButtonElement {
+    let wikiIconButton: HTMLButtonElement = document.createElement('button');
+    wikiIconButton.onclick = () => { window.open(wikiLink, '_blank'); }
+    wikiIconButton.title = 'Open Wiki page';
+    wikiIconButton.className = 'clickable fa fa-wikipedia-w';
+    wikiIconButton.setAttribute('aria-hidden', 'true');
+    return wikiIconButton;
+}
+
+export function aStoreIconButton(storeLink: string): HTMLButtonElement {
+    let storeIconButton: HTMLButtonElement = document.createElement('button');
+    storeIconButton.onclick = () => { window.open(storeLink, '_blank'); }
+    storeIconButton.title = 'Buy this art!';
+    storeIconButton.className = 'clickable fa fa-shopping-bag';
+    storeIconButton.setAttribute('aria-hidden', 'true');
+    return storeIconButton;
+}
+
+export function aNameIcon(): HTMLElement {
+    let nameIcon: HTMLElement = document.createElement('i');
+    nameIcon.title = 'Name';
+    nameIcon.className = 'fa fa-tag';
+    nameIcon.setAttribute('aria-hidden', 'true');
+    return nameIcon;
+}
+
+export function aSpeciesIcon(): HTMLElement {
+    let speciesIcon: HTMLElement = document.createElement('i');
+    speciesIcon.title = 'Species';
+    speciesIcon.className = 'fa fa-user';
+    speciesIcon.setAttribute('aria-hidden', 'true');
+    return speciesIcon;
+}
+
+export function aPersonalityIcon(): HTMLElement {
+    let personalityIcon: HTMLElement = document.createElement('i');
+    personalityIcon.title = 'Personality';
+    personalityIcon.className = 'fa fa-heart';
+    personalityIcon.setAttribute('aria-hidden', 'true');
+    return personalityIcon;
+}
+
+export function aCoffeeIcon(): HTMLElement {
+    let coffeeIcon: HTMLElement = document.createElement('i');
+    coffeeIcon.title = 'Favourite coffee';
+    coffeeIcon.className = 'fa fa-coffee';
+    coffeeIcon.setAttribute('aria-hidden', 'true');
+    return coffeeIcon;
+}
+
+export function aBirthdayIcon(villager: Villager): HTMLButtonElement | HTMLElement {
+    if (birthdayIsToday(villager.birthday.toString())) {
+        return aBirthdayButton(villager.name);
+    } else {
+        let birthdayIcon: HTMLElement = document.createElement('i');
+        birthdayIcon.title = 'Birthday';
+        birthdayIcon.className = 'fa fa-birthday-cake';
+        birthdayIcon.setAttribute('aria-hidden', 'true');
+        return birthdayIcon;
+    }
 }
