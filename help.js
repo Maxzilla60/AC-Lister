a23236660187b06694686d6e83c18b5a1cbef456
Maxime Orione <maxpower98@hotmail.com>
2019-06-03 14:16:49 +0200

    🔥 Removed unneeded files from merge

--- help.js
+++
@@ -1,6 +0,0 @@
-// Percentage of Villager heads done (run in browser console)
-var i = 0;
-for (v in villagers) {
-	if (villagers[v].head != "wip.jpg") i++;
-}
-Math.floor((i/villagers.length)*100) + "%"
