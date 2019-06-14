035ce0aa4f060d3ab9f8531fd666483e847e2d3e
Maxime Orione <maxpower98@hotmail.com>
2019-06-14 13:38:26 +0200

    🔊 Added debugging logs

--- src/components/ImageBuilder.ts
+++ src/components/ImageBuilder.ts
@@ -15,7 +15,12 @@ export default class ImageBuilder extends HTMLElementBuilder<HTMLImageElement> {
 
     private initFallBackSrc(src: string): void {
         this.element.src = src;
-        this.element.onerror = () => {
+        this.element.onerror = (event: Event) => {
+            if (src.includes('Cece') {
+                console.log('Error, event:', event);
+                console.log('Error, this:', this);
+                console.log('Error, loading:', src);
+            }
             // @ts-ignore
             this.src = src;
         };
