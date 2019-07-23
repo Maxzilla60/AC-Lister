40d8cbc0907a236b1f718f3e9fc0f5a983462b93
Maxime Orione <maxpower98@hotmail.com>
2019-07-23 14:43:09 +0200

    🔥 Removed trim function

--- src/util.ts
+++ src/util.ts
@@ -19,11 +19,6 @@ export function clearElement(element: HTMLElement): void {
     element.innerHTML = '';
 }
 
-// Trim '(2)' from name for duplicate names
-export function trimName(name: string): string {
-    return name.replace(' (2)', '');
-}
-
 export function aTextNode(text: string): Text {
     return document.createTextNode(text);
 }
