36d97c7a9c021d3804cb21ad026027684bc1c0eb
Maxime Orione <maxpower98@hotmail.com>
2019-11-13 17:56:37 +0100

    ✨ Implemented exportAsImage

--- src/viewer/controller.ts
+++ src/viewer/controller.ts
@@ -2,14 +2,18 @@ import VillagerList from '../models/villagerlist.model';
 import AppStateService from '../state/state.service';
 import { getElement as $, replaceChildren } from '../util';
 import ViewerComponents from './viewer.components';
+import { saveAs } from 'file-saver';
+import html2canvas from 'html2canvas';
 import lozad from 'lozad';
 
 export default class Controller {
-	private state: AppStateService;
 	private lozadObserver: lozad.Observer;
 	private lists: VillagerList[];
 
+	private listsElement: HTMLUListElement;
+
 	constructor() {
+		this.listsElement = $('lists') as HTMLUListElement;
 		this.lozadObserver = lozad();
 	}
 
@@ -17,11 +21,12 @@ export default class Controller {
 		const state = new AppStateService();
 		this.lists = state.getLists();
 		this.renderLists();
+		$('image_button').addEventListener('click', this.exportAsImage);
 	}
 
 	private renderLists(): void {
 		if (this.lists.length <= 0) {
-			replaceChildren($('lists'), ViewerComponents.aNoListInfoElement());
+			replaceChildren(this.listsElement, ViewerComponents.aNoListInfoElement());
 			return;
 		}
 
@@ -29,8 +34,18 @@ export default class Controller {
 		for (const list of this.lists) {
 			fragment.appendChild(ViewerComponents.aListElement(list));
 		}
-		replaceChildren($('lists'), fragment);
+		replaceChildren(this.listsElement, fragment);
 		this.observeLazyLoadedImages();
+	}
+
+	private exportAsImage(): void {
+		$('menu').style.display = 'none';
+		html2canvas($('main_container')).then((canvas) => {
+			canvas.toBlob((blob) => {
+				saveAs(blob, 'AnimalCrossing-VillagerLists.png');
+				$('menu').style.display = '';
+			});
+		});
 	}
 
 	private observeLazyLoadedImages(): void { this.lozadObserver.observe(); }
