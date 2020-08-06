994754e6ae4f15ad1d0629925a5ebccff3362098
Maxime Orione <maxpower98@hotmail.com>
2020-08-06 17:45:07 +0200

    🔥 Removed viewer page

--- src/viewer/init.ts
+++
@@ -1,9 +0,0 @@
-import VillagersRepository from '../shared/repository/villagers.repository';
-import Presenter from './presenter';
-
-const app = new Presenter();
-app.init();
-// tslint:disable-next-line: no-string-literal
-window['percentageOfVillagersWithProfileImage'] = VillagersRepository.percentageOfVillagersWithProfileImage;
-// tslint:disable-next-line: no-string-literal
-window['villagers'] = VillagersRepository.getAllVillagers();
