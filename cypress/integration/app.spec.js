10291ea0ce7286543e3ddf7bf2d3c4aadf4baf13
Maxime Orione <maxpower98@hotmail.com>
2020-09-03 17:30:27 +0200

    ✅ Implemented more Cypress tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,6 +1,18 @@
 describe('App', () => {
+	let testData;
 	const listName1 = 'Favourite Villagers';
 	const listName2 = 'My Town';
+
+	before(() => {
+		cy.fixture('testStateData.json').then(fixtureData => {
+			testData = fixtureData;
+		});
+	});
+
+	function loadTestData(key) {
+		localStorage.lists = JSON.stringify(testData[key]);
+		cy.visitPage();
+	}
 
 	beforeEach(() => {
 		cy.restoreLocalStorage();
@@ -38,5 +50,63 @@ describe('App', () => {
 		cy.get('#add_remove_button').click();
 
 		cy.get('.list').first().find('.list_member').should('have.length', 1);
+	});
+
+	it('should select list when clicking its title', () => {
+		loadTestData('twoEmptyLists');
+		const firstListId = 'a2a2383f-99fa-4546-a809-673b20017b13';
+		const secondListId = 'e6243c12-d8e5-40b1-8346-34be7644067f';
+
+		cy.get('#search_results')
+			.find('.result')
+			.first()
+			.click();
+
+		cy.get('.list_title').first().click();
+		cy.get('#list_select').should('have.value', firstListId);
+		cy.get('.list_title').eq(1).click();
+		cy.get('#list_select').should('have.value', secondListId);
+	});
+
+	it('should select member when clicking its icon in a list', () => {
+		cy.fixture('../../src/shared/repository/villagers.json').then(villagersArray => {
+			loadTestData('twoLists');
+			const firstListId = 'Uq8VlkaYGJGhlB2YJt7pY';
+			const secondListId = 'Qep1wh2jSmuVMBcNTPJbW';
+
+			cy.get('.list .list_member')
+				.first()
+				.children()
+				.first()
+				.click();
+			cy.get('#list_select').should('have.value', firstListId);
+			cy.get('#villager_information').should('contain', 'Scoot');
+			cy.get('.list .list_member')
+				.eq(1)
+				.children()
+				.first()
+				.click();
+			cy.get('#list_select').should('have.value', secondListId);
+			cy.get('#villager_information').should('contain', 'Stitches');
+		});
+	});
+
+	it('should perform the birthday easter egg', () => {
+		cy.clock(Date.UTC(2020, 2, 19), ['Date'])
+
+		cy.get('#search_bar').clear().type('birthday');
+		cy.waitForSearchDebounce();
+		cy.get('#search_bar').should('have.class', 'birthday');
+
+		cy.get('#search_results').find('.result').first()
+			.should('contain', 'Merengue')
+			.click();
+
+		cy.get('#birthday_button')
+			.should('exist')
+			.should('have.css', 'color', 'rgb(255, 105, 180)');
+		cy.get('#birthday_button')
+			.next()
+			.should('have.class', 'birthday');
 	});
 });
