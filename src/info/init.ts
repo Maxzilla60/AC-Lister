d08043e0d9e6d0c589b3c6c15deb4c186830d5a2
Maxime Orione <maxpower98@hotmail.com>
2021-10-26 14:37:26 +0200

    ⬆ Upgraded to Parcel 2

--- src/info/init.ts
+++
@@ -1,7 +0,0 @@
-import { html } from '../../public/README.md';
-import { getElement as $ } from '../shared/util';
-
-const childNodes = new DOMParser().parseFromString(html, 'text/html').body.childNodes;
-const articleElement = document.createElement('article');
-childNodes.forEach(n => articleElement.appendChild(n));
-$('readme_container').appendChild(articleElement);
